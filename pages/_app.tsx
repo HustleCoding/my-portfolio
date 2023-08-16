4853521eb130355e6f0895c63684b9c94e84a79f
FlorinDobinciuc <florin.dobiniciuc@thinslices.com>
2023-08-16 16:45:45 +0300

    Add an image componet for better performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,21 @@ import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { AppProps } from "next/app";
+import MyImage from '../components/MyImage'
+import { MDXProvider } from '@mdx-js/react';
 
 config.autoAddCss = false;
 
+const components = {
+  MyImage
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <MDXProvider components={components}>
+        <Component {...pageProps} />
+      </MDXProvider>
+  );
 }
 
 export default MyApp;
