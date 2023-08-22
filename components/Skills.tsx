e7a487d783ca72b08dd8638fd7214a610ebd3ff6
FlorinDobinciuc <florin.dobiniciuc@thinslices.com>
2023-08-22 21:20:48 +0300

    Refactor the components

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,78 +3,75 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
 import { faCode, faServer, faCloud } from "@fortawesome/free-solid-svg-icons";
 
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { y: 20, opacity: 0 },
+  visible: { y: 0, opacity: 1 },
+};
+
 const Skills: React.FC = () => {
-  const iconVariants = {
-    hover: { scale: 1.1, y: -10 },
-    initial: { scale: 1, y: 0 },
-  };
-
   return (
-    <section className="py-12 dynamic-gradient">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-center mb-8 text-white">
+    <motion.section
+      className="py-12 dynamic-gradient"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
+        <motion.h2 className="text-3xl font-bold text-center mb-8 text-white">
           Skills
-        </h2>
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-          <div className="text-center">
-            <motion.div
-              variants={iconVariants}
-              whileHover="hover"
-              initial="initial"
-            >
-              <FontAwesomeIcon
-                icon={faCode}
-                size="6x"
-                className="mb-4 text-white"
-              />
-            </motion.div>
-            <h3 className="text-2xl font-semibold mb-4 text-white">
-              Frontend Development
-            </h3>
-            <p className="text-white">
-              React, Next.js, JavaScript, TypeScript, HTML, CSS, Sass,
-              TailwindCSS, Storybook
-            </p>
-          </div>
+        </motion.h2>
 
-          <div className="text-center">
-            <motion.div
-              variants={iconVariants}
-              whileHover="hover"
-              initial="initial"
-            >
-              <FontAwesomeIcon
-                icon={faServer}
-                size="6x"
-                className="mb-4 text-white"
-              />
-            </motion.div>
-            <h3 className="text-2xl font-semibold mb-4 text-white">
-              Backend Development
-            </h3>
-            <p className="text-white">Node.js, Django, RESTful APIs, Python</p>
-          </div>
+        <div className="absolute inset-0 from-purple-400 to-purple-600 opacity-20"></div>
 
-          <div className="text-center">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 relative z-10">
+          {[
+            {
+              icon: faCode,
+              title: "Frontend Development",
+              description:
+                "React, Next.js, JavaScript, TypeScript, HTML, CSS, Sass, TailwindCSS, Storybook",
+            },
+            {
+              icon: faServer,
+              title: "Backend Development",
+              description: "Node.js, Django, RESTful APIs, Python",
+            },
+            {
+              icon: faCloud,
+              title: "Cloud & DevOps",
+              description: "AWS, Docker, CI/CD, Vercel",
+            },
+          ].map((skill) => (
             <motion.div
-              variants={iconVariants}
-              whileHover="hover"
-              initial="initial"
+              className="text-center"
+              variants={itemVariants}
+              whileHover={{ scale: 1.05, y: -5 }}
             >
               <FontAwesomeIcon
-                icon={faCloud}
+                icon={skill.icon}
                 size="6x"
-                className="mb-4 text-white"
+                className="mb-4 text-white hover:text-blue-400 transition-all duration-300"
               />
+              <h3 className="text-2xl font-semibold mb-4 text-white">
+                {skill.title}
+              </h3>
+              <p className="text-white">{skill.description}</p>
             </motion.div>
-            <h3 className="text-2xl font-semibold mb-4 text-white">
-              Cloud & DevOps
-            </h3>
-            <p className="text-white">AWS, Docker, CI/CD, Vercel</p>
-          </div>
+          ))}
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -11,7 +11,14 @@ type BlogProps = {
 };
 
 export const getStaticProps: GetStaticProps<BlogProps> = async () => {
-  const posts = getPosts();
+  let posts = getPosts();
+
+  posts = posts.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB.getTime() - dateA.getTime();
+  });
+
   return {
     props: {
       posts,
@@ -26,24 +33,43 @@ const Blog: React.FC<BlogProps> = ({ posts }) => (
       <link rel="icon" href="/favicon.svg" />
     </Head>
     <Header />
+
     <div className="bg-white min-h-screen pb-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <h1 className="text-4xl font-bold mb-8 text-black">Blog Posts</h1>
-        {posts.map((post) => (
-          <Link key={post.title} href={`/blog/${post.slug}`} passHref>
-            <div className="block mb-8 p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 cursor-pointer dynamic-gradient">
-              <h2 className="text-2xl font-semibold mb-4 text-white">
-                {post.title}
+        <h1 className="text-5xl font-extrabold mb-12 text-black">Blog</h1>
+
+        {posts[0] && (
+          <Link href={`/blog/${posts[0].slug}`} passHref>
+            <div className="block mb-12 p-8 rounded-lg shadow-lg hover:shadow-xl transition duration-300 transform hover:-translate-y-2 cursor-pointer">
+              <h2 className="text-3xl font-bold mb-4 text-black">
+                {posts[0].title}
               </h2>
-              <p className="text-white mb-4">
-                {new Date(post.date).toLocaleDateString("en-US")}
+              <p className="mb-4">
+                {new Date(posts[0].date).toLocaleDateString("en-US")}
               </p>
-              <p className="text-white">{post.excerpt}</p>
+              <p>{posts[0].excerpt}</p>
             </div>
           </Link>
-        ))}
+        )}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {posts.slice(1).map((post) => (
+            <Link key={post.title} href={`/blog/${post.slug}`} passHref>
+              <div className="block p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:-translate-y-1 dynamic-gradient cursor-pointer">
+                <h2 className="text-2xl font-semibold mb-4 text-white">
+                  {post.title}
+                </h2>
+                <p className="text-white mb-4">
+                  {new Date(post.date).toLocaleDateString("en-US")}
+                </p>
+                <p className="text-white">{post.excerpt}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
+
     <Footer className="bg-[#003366]" />
   </div>
 );
